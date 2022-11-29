738d84bb00349d13b57655dc7056cd9b73ff8ade
Myunghoon Ju <myunghoonju@gmail.com>
2022-11-29 22:46:39 +0900

    Update WelcomeController.java

--- src/main/java/com/njonecompany/web/controller/WelcomeController.java
+++ src/main/java/com/njonecompany/web/controller/WelcomeController.java
@@ -15,8 +15,6 @@ public class WelcomeController {
 
     @GetMapping("/")
     public String index(Model model) {
-        logger.debug("Welcome to njonecompany.com...");
-
         model.addAttribute("msg", getMessage());
         model.addAttribute("today", new Date());
         System.out.println(model.getAttribute("today"));
